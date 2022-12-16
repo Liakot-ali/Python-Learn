0bcbebbcf4edc2bbe24337a15e6606eae66c8758
Liakot-Ali <liakotali2k18@gmail.com>
2022-12-16 08:54:47 +0600

    Learning creating package and using package, random number, and working with directory

---
+++ Packages/shopping.py
@@ -0,0 +1,6 @@
+
+def total_amount(arr):
+    total = 0
+    for val in arr:
+        total += val
+    return total
--- day8.py
+++ day8.py
@@ -1,5 +1,9 @@
 
 import Utils
+from Packages import shopping as shop
+
+import random as ran
+from pathlib import Path
 
 numbers = [2, 4, 1, 65, 8, -3, 9, 0, 15, 45]
 
@@ -9,4 +13,20 @@ print("Maximum is", max)
 min = Utils.find_min(numbers)
 print("Minimum is", min)
 
+# Using packages
+total = shop.total_amount(numbers)
+print("Total amount is :", total)
 
+# Random Value
+print("\nRandom")
+print(ran.random())
+print(ran.randint(20, 58))
+print(ran.choice(numbers))
+
+# File
+path = Path("")
+createPath = Path("./New File")
+createPath.rmdir()
+print(path.absolute())
+for p in path.glob("*.py"):
+    print(p)
