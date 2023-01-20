5003012afee6df1b5e7405e26b9c5994c2873087
Liakot-Ali <liakotali2k18@gmail.com>
2023-01-20 19:51:41 +0600

    Implementing PCA using breast cancer dataset

--- Machine Learning/Dimensionality Reduction/Feature Selection/PCA.py
+++ Machine Learning/Dimensionality Reduction/Feature Selection/PCA.py
@@ -1,9 +1,38 @@
+# Implementing PCA using breast cancer dataset
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.datasets import load_breast_cancer
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
 
+# Load breast cancer dataset and set the data to dataframe
 data = load_breast_cancer()
 df = pd.DataFrame(data['data'], columns=data['feature_names'])
-from sklearn.preprocessing import StandardScaler
+
+# Standardize the data
 scaler = StandardScaler()
+scaler.fit(df)
+
+# Find the best components using PCA
+scaled_data = scaler.transform(df)
+pca = PCA(n_components=2)   # only 2 principal components will be selected
+pca.fit(scaled_data)
+
+# Get the components value
+# print(pca.explained_variance_, pca.explained_variance_ratio_)
+x_pca = pca.transform(scaled_data)
+# print(pca.get_feature_names_out())
+# print(pca.components_)
+# print(x_pca)
+
+# Plotting the value using two feature
+plt.scatter(x_pca[:, 0], x_pca[:, 1], c=data['target'])
+plt.show()
+
+# Showing in headmap
+df_comp = pd.DataFrame(pca.components_, columns=data['feature_names'])
+plt.figure(figsize=(10, 6))
+sns.heatmap(df_comp)
+plt.show()
