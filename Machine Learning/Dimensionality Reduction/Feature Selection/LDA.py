e5a16a5e41efdf5c8cc898a12f94ef2e54bf0e75
Liakot-Ali <liakotali2k18@gmail.com>
2023-01-29 20:39:20 +0600

    Implementing LDA with iris dataset

--- Machine Learning/Dimensionality Reduction/Feature Selection/LDA.py
+++ Machine Learning/Dimensionality Reduction/Feature Selection/LDA.py
@@ -1,19 +1,31 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.datasets import load_breast_cancer
+from sklearn.datasets import load_iris
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import cross_val_score
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
-cancer = load_breast_cancer()
+# Import Iris dataset and store the data and target
+iris = load_iris()
+X = np.array(iris['data'])
+y = np.array(iris['target'])
 
-data = pd.DataFrame(data=cancer['data'], columns=cancer['feature_names'])
-print(data.keys())
+# Train and fit the model
+lda = LinearDiscriminantAnalysis()
+lda.fit(X, y)
 
-scaler = StandardScaler()
-scaler.fit(data)
+# Transform the data
+data_set = lda.transform(X)
+print(data_set)
 
-scaled_data = scaler.transform(data)
+# Plotting the grouped data
+plt.scatter(data_set[y == 0, 0], data_set[y == 0, 1], c='r', label=iris['target_names'][0])
+plt.scatter(data_set[y == 1, 0], data_set[y == 1, 1], c='g', label=iris['target_names'][1])
+plt.scatter(data_set[y == 2, 0], data_set[y == 2, 1], c='b', label=iris['target_names'][2])
+plt.legend()
+plt.show()
 
-lda = LinearDiscriminantAnalysis(n_components=2)
-
+# Predicting score
+score = cross_val_score(lda, X, y, scoring='accuracy')
+print(np.mean(score))
